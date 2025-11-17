ad89228d2813d8d8711fff26b13f540a9151affe
elmira1385 <elmirashokriyan64@gmail.com>
2025-11-17 20:48:07 +0330

    v6

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: "https://api.jioresume.com",
 });
 
 instance.interceptors.request.use((config) => {
